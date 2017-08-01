9d7069683a0155cfe479b253f9d5415958a84923
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-08-01 16:38:02 -0700

    implemented BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 /**
- * Import the stylesheet you want used! Here we just reference
- * the main SCSS file we have in the styles directory.
- */
+* Import the stylesheet you want used! Here we just reference
+* the main SCSS file we have in the styles directory.
+*/
 import './styles/main.scss';
 
 /**
- * Both configureStore and Root are required conditionally.
- * See configureStore.js and Root.js for more details.
- */
+* Both configureStore and Root are required conditionally.
+* See configureStore.js and Root.js for more details.
+*/
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
 
 const store = configureStore();
 
 ReactDOM.render(
-  <Root store={store} />,
+  <BrowserRouter>
+    <Root store={store} />
+  </BrowserRouter>,
   document.getElementById('root')
 );
