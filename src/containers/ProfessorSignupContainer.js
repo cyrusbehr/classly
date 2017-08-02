1d5074ec92d9a3c743023dee8e5defe1f9966212
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-08-01 17:24:18 -0700

    populated containers

---
+++ src/containers/ProfessorSignupContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class ProfessorSignupContainer extends Component {
+  render() {
+    return(
+      <div>
+        This is the login container
+      </div>
+    )
+  }
+};
--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
+import LoginContainer from './LoginContainer'
+import MainViewContainer from './MainViewContainer'
+import ProfessorSignupContainer from './ProfessorSignupContainer'
+import StudentSignupContainer from './StudentSignupContainer'
 
 export default class Routes extends Component {
 
    render() {
      return(
        <Switch>
-         {/* Routes go here */}
+         <Route path="/" exact={true} component={LoginContainer}/>
        </Switch>
      )
    }
