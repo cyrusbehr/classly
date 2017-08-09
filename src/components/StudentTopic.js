573b11050ddd04f2ed83630d59f722e018b88952
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-08-09 12:16:51 -0700

    moved delete button on topic card

--- src/components/StudentTopic.js
+++ src/components/StudentTopic.js
@@ -100,7 +100,12 @@ class StudentTopic extends Component {
             style={this.state.hover || this.state.alreadyClicked ? {color: '#FF7E65'} : {color:'#30383E'}}
           >{this.state.votes}</div>
         </div>
-        {isCreatorOrProfessorOrTA ? <button onClick={(e)=> this.deleteItem(e)}>delete</button> : null }
+        {isCreatorOrProfessorOrTA
+          ?
+          <svg className="delete-topic" onClick={(e)=> this.deleteItem(e)} width="40px" height="40px">
+            <path d="M13.172 16L.586 3.414c-.78-.78-.78-2.047 0-2.828.78-.78 2.048-.78 2.828 0L16 13.172 28.586.586c.78-.78 2.047-.78 2.828 0 .78.78.78 2.047 0 2.828L18.828 16l12.586 12.586c.78.78.78 2.047 0 2.828-.78.78-2.048.78-2.828 0L16 18.828 3.414 31.414c-.78.78-2.047.78-2.828 0-.78-.78-.78-2.047 0-2.828L13.172 16z"/>
+          </svg>
+          : null }
       </div>
     );
   }
