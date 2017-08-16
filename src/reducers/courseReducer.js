2c0ef7b5397b33ce01434ec5327c9c5a7f81597b
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-08-16 16:50:48 -0700

    dashboard WIP

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,18 @@
-initialState = [];
+var initialState = [];
+import { ADD_COURSE } from '../constants/ActionTypes';
+
 //action that gets passsed in will be of type TOGGLE_FILTER
 //and have a string attached that is the new filter
 
 export default function (state = initialState, action) {
   switch (action.type) {
-  
+
+    case ADD_COURSE:
+    //slice deep copy
+    var newState = state.slice(0, state.length);
+    //push new course onto that array
+    newState.push(action.courseObject);
+    return newState;
   default:
     return state;
   }
