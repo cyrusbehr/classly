6ca101e2cfa220b79edcb0a816ed9a9babf91e84
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-08-16 23:06:59 -0700

    Create Class works

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -9,7 +9,7 @@ export default function (state = initialState, action) {
 
     case ADD_COURSE:
     //slice deep copy
-    var newState = [...state]
+    var newState = [...state];
     newState.push(action.courseObject);
     return newState;
 
